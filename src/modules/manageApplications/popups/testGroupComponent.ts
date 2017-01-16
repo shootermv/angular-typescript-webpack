1f7c693b6c607d7ca57f225a4f33d9c517c5ba3b
Moshe <shootermv@gmail.com>
2017-01-16 17:54:57 +0200

    move between active and tosave

--- src/modules/manageApplications/popups/testGroupComponent.ts
+++ src/modules/manageApplications/popups/testGroupComponent.ts
@@ -2,7 +2,8 @@ import {ManageApplicationsService} from './../manageApplicationsService'
 class Ctrl {
    public showTestGroup:boolean;  
    public static $inject:string[] = ['manageApplicationsService'];
-   public activeValues:any;
+   public activeValues:any[] = [];
+   public toSaved:any[] = [];
    public byParam:any = '14';
    public dataList:Object = {14:'Hostname',1:'Username',18:'Business Location'};
    constructor(public manageApplicationsService:ManageApplicationsService){}
@@ -24,6 +25,22 @@ class Ctrl {
             return obj;
         });     
       });       
+   }
+
+   move(orig, current) {
+      orig.filter(b=>b.$selected).forEach(item=>{
+          current.unshift(item);
+          orig.splice(orig.indexOf(item),1);
+      }) 
+   }
+
+
+   moveToSaved() {
+      this.move(this.activeValues, this.toSaved)      
+   }
+
+   moveToActive() {
+     this.move(this.toSaved, this.activeValues)   
    }
 
 };
