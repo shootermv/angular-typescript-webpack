62c68b5c709d8559cf993878b091443b772a0b3f
Moshe <shootermv@gmail.com>
2017-01-17 09:40:46 +0200

    appService code prettified

--- src/modules/manageApplications/manageApplicationsService.ts
+++ src/modules/manageApplications/manageApplicationsService.ts
@@ -1,30 +1,26 @@
-
-    import IHttpService = angular.IHttpService;
-    import IPromise = angular.IPromise;
-    import IHttpPromise = angular.IHttpPromise;
-    import IQService = angular.IQService;
-    import IDeferred = angular.IDeferred;
-    import IHttpPromiseCallbackArg = angular.IHttpPromiseCallbackArg;
-
-
-    export class ManageApplicationsService {
+import IHttpService = angular.IHttpService;
+import IPromise = angular.IPromise;
+import IHttpPromise = angular.IHttpPromise;
+import IQService = angular.IQService;
+import IDeferred = angular.IDeferred;
+import IHttpPromiseCallbackArg = angular.IHttpPromiseCallbackArg;
 
 
-        static $inject:string[] = ["$http", "$q"];
-
-        constructor(private $http:IHttpService, private $q:IQService) {
-        }
+export class ManageApplicationsService {
 
 
+    static $inject:string[] = ["$http", "$q"];
 
-        public getMetaData():IPromise<any> {
-            return this.$http.get('/api/appView/appTree');
-        }
-        public loadAccountTestGroup() :IPromise<any>{          
-          return this.$http.get('/api/testGroups/testGroup?accountId=' + 1);
-        }
-        public findBySearchCriteria(saId):IPromise<any>{
-          return  this.$http.get('/api/testGroups/findBySearchCriteria?accountId=' + 1+'&query=&saId='+saId); 
-        }
+    constructor(private $http:IHttpService, private $q:IQService) {}
+    
+    public getMetaData():IPromise<any> {
+        return this.$http.get('/api/appView/appTree');
     }
+    public loadAccountTestGroup() :IPromise<any>{          
+        return this.$http.get('/api/testGroups/testGroup?accountId=' + 1);
+    }
+    public findBySearchCriteria(saId):IPromise<any>{
+        return  this.$http.get('/api/testGroups/findBySearchCriteria?accountId=' + 1+'&query=&saId='+saId); 
+    }
+}
 
