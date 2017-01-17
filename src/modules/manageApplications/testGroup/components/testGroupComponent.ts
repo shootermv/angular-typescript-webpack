636c3af91ec667ff3f7e59b3c10edcf3f53769d4
Moshe <shootermv@gmail.com>
2017-01-17 10:57:51 +0200

    added two more methods to service

--- src/modules/manageApplications/testGroup/components/testGroupComponent.ts
+++ src/modules/manageApplications/testGroup/components/testGroupComponent.ts
@@ -5,25 +5,49 @@ class Ctrl {
    public activeValues:any[] = [];
    public toSaved:any[] = [];
    public byParam:any = '14';
+   public testGroupVersion:number;
    public dataList:Object = {14:'Hostname',1:'Username',18:'Business Location'};
+
+   private criterion:Object = {
+        "staticAttrId": 0,
+        "criterionValues": []
+    }
+
+
+
+
    constructor(public testGroupService:TestGroupService){}
 
    
    $onInit() {
-      this.byParamChanged(14);
+
+     this.testGroupService.loadAccountTestGroup(1)
+     .then((response)=>{
+
+        this.testGroupVersion = response.data.version; 
+        this.byParamChanged(response.data.staticAttrCriterionDTOList[0].staticAttrId);
+
+        this.toSaved = response.data.staticAttrCriterionDTOList[0].criterionValues.map(val=>{
+           return {value:val, $selected : false};
+        })
+     })
+
+
+
+      
    }
 
    closeDialog() {
        this.showTestGroup = false;
    }
 
-   byParamChanged(val) {      
+   byParamChanged(val) {    
        this.testGroupService.findBySearchCriteria(val)
       .then((response:any) => {
         this.activeValues = response.data.values.map(val=>{
             let obj = {value:val, $selected : false};
             return obj;
-        });     
+        });    
       });       
    }
 
@@ -43,6 +67,12 @@ class Ctrl {
      this.move(this.toSaved, this.activeValues)   
    }
 
+   saveTestGroup() {
+      this.criterion["staticAttrId"] = this.byParam;
+      this.criterion["criterionValues"] = this.toSaved.map(item => item.value)
+      this.testGroupService.saveTestGroup(1, this.criterion, 0)
+   }
+   
 };
 export class TestGroup implements ng.IComponentOptions {
     public template:string = require('./testGroupComponent.html');
--- src/modules/manageApplications/testGroup/core/services/testGroupService.ts
+++ src/modules/manageApplications/testGroup/core/services/testGroupService.ts
@@ -7,8 +7,14 @@ import IQService = angular.IQService;
 export class TestGroupService {
     static $inject:string[] = ["$http", "$q"];
     constructor(private $http:IHttpService, private $q:IQService) {}
-    
+    public loadAccountTestGroup(accountId):IPromise<any> {
+        return this.$http.get('/api/testGroups/testGroup?accountId=' + accountId);
+    }    
     public findBySearchCriteria(saId):IPromise<any>{
-        return  this.$http.get('/api/testGroups/findBySearchCriteria?accountId=' + 1+'&query=&saId='+saId); 
+        return this.$http.get('/api/testGroups/findBySearchCriteria?accountId=' + 1+'&query=&saId='+saId); 
     }
+    public saveTestGroup (accountId, dataObj, version):IPromise<any> {
+        return this.$http.post('/api/testGroups/saveOrUpdateTestGroup?accountId=' + encodeURIComponent(accountId)
+                        + '&version=' + encodeURIComponent(version), dataObj) ;
+    }    
 }
